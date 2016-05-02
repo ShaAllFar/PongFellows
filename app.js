8bd84b720629d252c995227c6a573e851b8ea1c1
sungkim3 <sng.kim103@gmail.com>
2016-05-02 15:46:33 -0700

    updates app.js to include storage of the active user to local storage

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var allUsers = [];
 var userLogin = document.getElementById('sign-in-form');
 var createUser = document.getElementById('sign-up-form');
+var homeName = document.getElementById('home-name');
+//User Object Constructor
 function UserObject(name, password) {
   this.userName = name;
   this.password = password;
   this.opponentsArray = [];
 }
-
+//Handles Create User Event, includes validation of existing Users
 function handleCreateUserEvent(event) {
   event.preventDefault();
   var foundUserName = false;
-  var name = event.target.signUpName.value.toString();
-  var password = event.target.signUpPassword.value.toString();
+  var newName = event.target.signUpName.value.toString();
+  var newPassword = event.target.signUpPassword.value.toString();
   if (allUsers.length > 0) {
     for (var i = 0; i < allUsers.length; i++) {
       if (name === allUsers[i].userName) {
@@ -23,7 +25,9 @@ function handleCreateUserEvent(event) {
     alert('Sorry!  This username already exists.  Please try again.');
   }
   if (!foundUserName) {
-    var CreateNewUser = new UserObject(name, password);
+    //store new user into local storage
+    localStorage.setItem('storedActiveUser', JSON.stringify(newName));
+    var CreateNewUser = new UserObject(newName, newPassword);
     allUsers.push(CreateNewUser);
     localStorage.setItem('storedUsers', JSON.stringify(allUsers));
     window.location = 'setup.html';
@@ -31,33 +35,44 @@ function handleCreateUserEvent(event) {
   event.target.newUserName.value = null;
   event.target.newPassword.value = null;
 }
-
+//Handles User Login Event through Validation
 function handleValidateEvent(event) {
   event.preventDefault();
-  var name = event.target.signInName.value.toString();
-  var password = event.target.signInPassword.value.toString();
+  var userExists = false;
+  var logInName = event.target.signInName.value.toString();
+  var logInPassword = event.target.signInPassword.value.toString();
   if (allUsers.length > 0) {
     for (var i = 0; i < allUsers.length; i++) {
-      if (name === allUsers[i].userName && password === allUsers[i].password) {
-        window.location = 'setup.html';
-      } else {
-        alert('Incorrect username or password.  Please try again.');
+      if (logInName === allUsers[i].userName && logInPassword === allUsers[i].password) {
+        userExists = true;
       }
+    }
+    if (userExists) {
+      //store new user into local storage
+      localStorage.setItem('storedActiveUser', JSON.stringify(name));
+      window.location = 'setup.html';
     }
   } else {
     alert('Incorrect username or password.  Please try again.');
   }
-  event.target.userName.value = null;
-  event.target.password.value = null;
+  event.target.signInName.value = null;
+  event.target.signInPassword.value = null;
 }
-if (createUser) {
-  userLogin.addEventListener('submit', handleValidateEvent);
-  createUser.addEventListener('submit', handleCreateUserEvent);
-}
-
+//Checks for existing local storage and updates stored data to allUser array
 (function checkLocal() {
   if (localStorage.storedUsers) {
     var parsedStoredUsers = JSON.parse(localStorage.storedUsers);
     allUsers = parsedStoredUsers;
   }
+  // console.log(allUsers);
+  return allUsers;
 })();
+
+homeName.textContent = JSON.parse(localStorage.storedActiveUser);
+//Event listeners
+if (createUser) {
+  createUser.addEventListener('submit', handleCreateUserEvent);
+}
+if (userLogin) {
+  userLogin.addEventListener('submit', handleValidateEvent);
+}
